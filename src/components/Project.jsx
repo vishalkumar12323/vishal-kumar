2038367d81eb21fe70e41b2de81e8296177ab20c
Vishal Kumar <vishalninaniya8949info@gmail.com>
2025-04-08 10:35:51 +0530

    fix css for project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -38,24 +38,24 @@ const Card = ({ project }) => {
           className="img select object-cover w-full h-auto bg-center"
         />
       </div>
-      <div className="card-overlay absolute top-0 left-0 w-full h-full z-[1] opacity-[1] transition-all">
+      <div className=" card-overlay absolute top-0 left-0 w-full h-full z-[1] opacity-[1] transition-all">
         <div className="info hidden absolute gap-[5px] left-12 bottom-8">
-          <div className="name text-[1.5rem] font-normal text-slate-900 uppercase">
+          <div className="name text-[1.1rem] md:text-[1.5rem] font-semibold text-slate-900 uppercase">
             {project.name}
           </div>
-          <div className="description capitalize text-[16px] font-normal max-w-[85%] text-slate-900">
+          <div className="description capitalize text-[10px] md:text-[16px] font-normal max-w-full  md:max-w-[85%] text-slate-900">
             {project.description}
           </div>
           <div className="projects-links flex gap-2 mt-[5px]">
             <a href={project.githubLink} target="_blank" className="github">
-              <IoLogoGithub size={30} />
+              <IoLogoGithub size={20} />
             </a>
             <a
               href={project.previewLink}
               target="_blank"
               className="preview-link"
             >
-              <IoOpenOutline size={30} />
+              <IoOpenOutline size={20} />
             </a>
           </div>
         </div>
